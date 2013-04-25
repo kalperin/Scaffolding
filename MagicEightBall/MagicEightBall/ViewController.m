402d95214fa7d9b2f498865e69cbdae207d650d7
Don Bora <dbora@mac.com>
2013-04-24 19:41:41 -0500

    adding MagicEightBall

---
+++ MagicEightBall/MagicEightBall/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  MagicEightBall
+//
+//  Created by Don Bora on 4/24/13.
+//  Copyright (c) 2013 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    __weak IBOutlet UILabel *answerLabel;
+}
+- (IBAction)shake:(id)sender;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)shake:(id)sender
+{
+    answerLabel.text = @"needs work!";
+}
+@end
